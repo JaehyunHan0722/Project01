6e66e55dab0fb53216b929d0f5a0a7aa99390330
Jaehyun Han <wogusgks@gmail.com>
2021-10-03 13:11:07 +0900

    combine to one folder

---
+++ Sign_In/userinfoDTO.java
@@ -0,0 +1,53 @@
+package project01;
+
+public class userinfoDTO {
+	private String userid;
+	private String password;
+	private String name;
+	private String cellnum;
+	
+	//insert
+	public userinfoDTO(String userid, String password, String name, String cellnum) {
+		super();
+		this.userid = userid;
+		this.password = password;
+		this.name = name;
+		this.cellnum = cellnum;
+	}
+//	//password_update
+//	public project_01DTO(String userid, String password) {
+//		super();
+//		this.userid = userid;
+//		this.password = password;	
+//	}
+//	//delete
+//	public project_01DTO(String userid) {
+//		super();
+//		this.userid = userid;
+//	}
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCellnum() {
+		return cellnum;
+	}
+	public void setCellnum(String cellnum) {
+		this.cellnum = cellnum;
+	}
+}
